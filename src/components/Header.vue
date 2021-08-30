97566108e1d38e30fd6c443935d015ab913ef2d5
ariel.plasencia <arielplasencia00@gmail.com>
2021-08-30 15:05:14 -0400

    add component Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-center">
+        <img alt="Vue logo" src="@/assets/logo.png">
+        <h1>Fruits</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Header'
+    }
+</script>
