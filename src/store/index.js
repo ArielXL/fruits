e48cbf87e5c5d7d8745b6619c683788c9d664c05
ariel.plasencia <arielplasencia00@gmail.com>
2021-08-30 23:06:43 -0400

    add states and mutations to store

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    fruits: [
+      { name: 'Orange', count: 0 },
+      { name: 'Apple', count: 0 },
+      { name: 'Pear', count: 0 },
+      { name: 'Pineapple', count: 0 },
+      { name: 'Peach', count: 0 }
+    ]
   },
   mutations: {
+    incrementCount(state, idx) {
+      state.fruits[idx].count += 1
+    },
+    resetCount(state) {
+      state.fruits.forEach( (fruit) => {
+        fruit.count = 0
+      })
+    }
   },
   actions: {
   },
